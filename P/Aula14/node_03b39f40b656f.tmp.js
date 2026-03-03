2d35e4a68c0acf6831004c6fda3d58279a6c9862
Henrique Branco <henriquebrancodasilvadias@yahoo.com>
2026-03-03 17:51:04 -0300

    Configurando Pastas

--- P/Aula14/node_03b39f40b656f.tmp.js
+++
@@ -1,5 +0,0 @@
-var corinthians = 1
-
-while (corinthians <= 4){
-    console.log(`O Corinthians tem ${4} Copas Do Brasil`)
-}
--- P/Aula16/funcao04.js
+++
@@ -1,8 +0,0 @@
-function fatorial(n){
-    let fat = 1
-    for (let contador = n; contador > 1; contador--){
-        fat *= contador
-    }
-    return fat
-}
-console.log(fatorial(5))
